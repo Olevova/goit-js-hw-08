006f15d8a21e24950ee398b0774f84d6e649f7d4
Olevova <olevova1983@gmail.com>
2022-10-05 18:28:56 +0300

    made gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,52 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery")
+// console.log(galleryEl);
+
+// реалызуэмо створення галереї на класі
+class ImgGallery{
+    constructor(galleryItems) {
+        this.galleryItems = galleryItems
+    }
+
+    makeGalleryEL() {
+        console.log(this.galleryItems);
+        const { preview, original, description } = this.galleryItems;
+        const galleryObj = this.galleryItems.map(({ preview, original, description }) => 
+            `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>`
+        ).join("")
+        return galleryObj
+    }
+
+    makeGallery(elem) {
+        elem.insertAdjacentHTML("beforeend", this.makeGalleryEL())
+        
+    }
+}
+
+//створюємо саму галерею
+
+const myGallery = new ImgGallery(galleryItems);
+console.log(myGallery.makeGallery(galleryEl))
+
+
+galleryEl.addEventListener("click", onClickImgMakeGallery)
+
+function onClickImgMakeGallery (event) {
+    event.preventDefault();
+    if(event.target.nodeName !== "IMG") {
+    return
+  }
+}
+
+let gallery = new SimpleLightbox('.gallery a',{captionsData:"alt", captionDelay: 250});
+gallery.on('show.simplelightbox')
+
+
