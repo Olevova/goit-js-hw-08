3e47c964cc96765b38bdf2b64ab28271fee339c9
Olevova <olevova1983@gmail.com>
2022-10-08 13:23:21 +0300

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,46 +1,37 @@
+// імпортуємо потрібні модулі для роботи
 import Player from '@vimeo/player';
+const loadash = require("lodash");
+
+// виділяємо плеєр та вішаємо на нього подію, за допомогою тротель робимо відслідковування події раз в секунду
 const iframe = document.querySelector('iframe');
-console.log(iframe);
+// console.log(iframe);
 
 const player = new Vimeo.Player(iframe);
-console.log(player);
+// console.log(player);
 
-myTime = {};
+player.on('timeupdate', loadash.throttle(function (event) {
+    const { seconds, percent, duration } = event;
+    const stopTime= { seconds, percent, duration };
+    console.log(stopTime);
+    return localStorage.setItem ("videoplayer-current-time", JSON.stringify(stopTime))
+}, 1000))
 
-player.on('timeupdate', function (event) {
-    console.log("for", myTime);
-        const { seconds, percent, duration } = event;
-    myTime = { seconds, percent, duration }
-    console.log(myTime);
-    return myTime
-})
+// Запускаємо відео зсилаючись на час, з локалсторідж
 
+const timeBegin = JSON.parse(localStorage.getItem("videoplayer-current-time"));
 
-// console.log("its",  myTime);
+console.log(timeBegin.seconds);
 
-// player.on('timeupdate', function (event) {
-        
-    
-//     myTime['videoplayer- current - time'] = event.seconds;
-//     console.log(myTime);
-//     return myTime
-// });
+player.setCurrentTime(timeBegin.seconds).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
 
-
-
-// player.setCurrentTime(myTime).then(function(seconds) {
-//     // seconds = the actual time that the player seeked to
-// }).catch(function(error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
-
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
-//     player.getVideoTitle().then(function(title) {
-//         console.log('title:', title);
-//     });
+        default:
+            // some other error occurred
+            break;
+    }
+});
