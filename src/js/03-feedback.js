0033c45c55637114e84501f9475bd17e475cf99f
Olevova <olevova1983@gmail.com>
2022-10-09 20:56:30 +0300

    new part

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,32 @@
-console.log("rrrrr");
-const my = document.querySelector('form')
-console.log(my);
-console.log("ffffff")
+const loadash = require("lodash");
+const formEl = document.querySelector('form');
+const inputEl = document.querySelector("input");
+const textEl = document.querySelector("textarea")
 
-const clientInfo = { email:null, massage:null};
+const clientInfo = {};
 
-my.addEventListener("input", (event) => {
-    
-    if(event.target.nodeName === 'INPUT') {
+formEl.addEventListener("input", loadash.throttle((event) => {
+    console.log(event.currenTarget, event.target);
+    if (event.target.nodeName === 'INPUT') {
         const email = event.target.value;
         clientInfo.email = email;
+    }
 
-    };
-    event.target.nodeName === 'TEXTAREA'
-    const massage = event.target.value;
-    clientInfo.massage = massage;
-
-
-   
-
+    if (event.target.nodeName === 'TEXTAREA') {
+        const massage = event.target.value;
+        clientInfo.massage = massage;
+    }
+    console.log(event);
     console.log(clientInfo);
     return localStorage.setItem("feedback-form-state", JSON.stringify(clientInfo))
-})
+}), 500)
+
+const dataValue = JSON.parse(localStorage.getItem("feedback-form-state"));
+// inputEl.value = dataValue.email;
+
+console.log(dataValue, textEl, dataValue.email);
+
+if (dataValue) {
+    inputEl.value = dataValue.email;
+    textEl.value = dataValue.massage;
+}
