e722b07249149545d4c42a8c9198540b525b4066
Olevova <olevova1983@gmail.com>
2022-10-09 13:02:48 +0300

    third

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+console.log("rrrrr");
+const my = document.querySelector('form')
+console.log(my);
+console.log("ffffff")
 
+const clientInfo = { email:null, massage:null};
+
+my.addEventListener("input", (event) => {
+    
+    if(event.target.nodeName === 'INPUT') {
+        const email = event.target.value;
+        clientInfo.email = email;
+
+    };
+    event.target.nodeName === 'TEXTAREA'
+    const massage = event.target.value;
+    clientInfo.massage = massage;
+
+
+   
+
+    console.log(clientInfo);
+    return localStorage.setItem("feedback-form-state", JSON.stringify(clientInfo))
+})
